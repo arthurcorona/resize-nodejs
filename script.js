5744b7e07eb3a8c194292715e4811acb49e0fa93
Arthur Corona <coronaggp@gmail.com>
2022-07-21 16:04:56 -0300

    installed compressImages

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+const sharp = require('sharp')
+
 let path = process.argv[2]
 let width = Number(process.argv[3])
 
 function resize(path, width) {
-    
+    sharp(path).resize({width: width}).toFile('./temporary_directory/output_resize.jpg', (error) => {
+        if(error) {
+            console.log(error);
+        }
+        else {
+            console.log("Deu tudo certo!");
+        }
+    })
 }
+
+resize(path, width)
